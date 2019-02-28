62d0936f54e4ca35ab4229b2419314f52f306a9f
Stephen <stephen@thoughtstem.com>
2019-02-28 12:15:35 -0800

    adding ratchet

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 (define collection "ts-all-dev")
 (define deps '("base"
                "https://github.com/thoughtstem/racket-chipmunk.git"  
+               "https://github.com/thoughtstem/ratchet.git"  
                "https://github.com/thoughtstem/game-engine.git" 
                "https://github.com/thoughtstem/game-engine-demos.git?path=game-engine-demos-common" 
                "https://github.com/thoughtstem/game-engine-rpg.git" 
