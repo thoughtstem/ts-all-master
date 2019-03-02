7293e52ea68db3f511a1b74d5bf53e6f60624e67
Stephen <stephen@thoughtstem.com>
2019-03-02 10:19:53 -0800

    bug in travis yml

--- info.rkt
+++ info.rkt
@@ -16,7 +16,6 @@
                "https://github.com/thoughtstem/TS-Languages.git?path=k2"
                ))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/ts-all-dev.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(thoughtstem))
